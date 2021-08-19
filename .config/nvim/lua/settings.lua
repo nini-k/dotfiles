5fd48c729ce2ecec519b1c844060677a0305a683
Nikolay Kolesnikov <kolesnikov.nini@gmail.com>
2021-08-19 11:47:14 +0300

    formatting and swapfile off

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -8,39 +8,39 @@
 
 -- This file can be loaded by calling `require('module_name')` from your init.lua
 
-local cmd  = vim.cmd     	   -- execute Vim commands
-local exec = vim.api.nvim_exec -- execute Vimscript
-local fn   = vim.fn       	   -- call Vim functions
 local g    = vim.g         	   -- global variables
 local o    = vim.o         	   -- global options
 local b    = vim.bo        	   -- buffer-scoped options
 local w    = vim.wo        	   -- windows-scoped options
+local fn   = vim.fn       	   -- call Vim functions
+local cmd  = vim.cmd     	   -- execute Vim commands
+local exec = vim.api.nvim_exec -- execute Vimscript
 
 o.termguicolors = true       -- enable 24-bit RGB colors
 cmd([[colorscheme molokai]]) -- set colorscheme
 
-o.mouse     = 'a'           -- enable mouse support
-o.bs        = '2'           -- enable mouse support
-o.clipboard = 'unnamedplus' -- copy/paste to system clipboard
-b.swapfile  = false         -- don't use swapfile
+o.bs        = "2"           -- enable mouse support
+o.mouse     = "a"           -- enable mouse support
+o.swapfile  = false         -- don't use swapfile
+o.clipboard = "unnamedplus" -- copy/paste to system clipboard
 
 o.hidden     = true -- enable background buffers
 o.history    = 100  -- remember n lines in history
-o.lazyredraw = true -- faster scrolling
 b.synmaxcol  = 320  -- max column for syntax highlight
+o.lazyredraw = true -- faster scrolling
 
+o.tabstop     = 4    -- 1 tab == 4 spaces
 o.expandtab   = true -- use spaces instead of tabs
 o.shiftwidth  = 4    -- shift 4 spaces when tab
-o.tabstop     = 4    -- 1 tab == 4 spaces
 o.smartindent = true -- autoindent new lines
 
-o.syntax     = 'enable' -- enable syntax highlighting
+o.syntax     = "enable" -- enable syntax highlighting
 w.number     = true     -- show line number
 o.showmatch  = true     -- highlight matching parenthesis
-w.foldmethod = 'syntax' -- enable folding (default 'foldmarker')
+o.smartcase  = true     -- ignore lowercase for the whole pattern
+w.foldmethod = "syntax" -- enable folding (default "foldmarker")
 o.splitright = true     -- vertical split to the right
 o.splitbelow = true     -- orizontal split to the bottom
 o.ignorecase = true     -- ignore case letters when search
-o.smartcase  = true     -- ignore lowercase for the whole pattern
 
--- w.colorcolumn = '120' -- line lenght marker at 80 columns
+-- w.colorcolumn = "120" -- line lenght marker at 80 columns
