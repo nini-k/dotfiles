e28f42b6c8c00d9e0a01617b8db14e62ff828638
Nikolay Kolesnikov <kolesnikov.nini@gmail.com>
2021-08-29 20:05:35 +0300

    tsserver disabled

--- .config/nvim/lua/lsp/servers/tsserver.lua
+++ .config/nvim/lua/lsp/servers/tsserver.lua
@@ -17,6 +17,7 @@ return function(language_server_path, on_attach)
     local bin_path = language_server_path .. "/typescript-language-server"
     return {
         cmd       = {bin_path, "--stdio" },
+        settings  = { documentFormatting = false },
         on_attach = function(client, bufnr)
             on_attach(client, bufnr)
             
